153ef5352a29175f23ce71877917ea43b87d832f
gokulsidd <gokulsidharth02@gmail.com>
2025-06-17 16:26:36 +0530

    customized button and input components

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,9 +10,9 @@ const buttonVariants = cva(
     variants: {
       variant: {
         primary:
-          "bg-gradient-to-b from-blue-600 to-blue-700 text-primary-foreground hover:from-blue-700 hover:to-blue-700",
+          "bg-blue-500 text-primary-foreground hover:bg-blue-600",
         destructive:
-          "bg-gradient-to-b from-amber-600 to-amber-700 text-primary-foreground hover:from-amber-700 hover:to-amber-700",
+          "bg-amber-700 text-primary-foreground hover:bg-amber-800",
         outline:
           "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
         secondary:
@@ -20,10 +20,10 @@ const buttonVariants = cva(
         ghost:
           "border-transparent shadow-none hover:bg-accent hover:text-accent-foreground",
         muted: "bg-neutral-200 text-neutral-600 hover:bg-neutral-200/80",
-        teritary: "bg-blue-100 text-blue-600 border-transparent hover:bg-blue-200 shadow-none"
+        teritary: "bg-blue-100 text-blue-500 border-transparent hover:bg-blue-200 shadow-none"
       },
       size: {
-        default: "h-8 px-4 py-2 has-[>svg]:px-3",
+        default: "h-9 px-4 py-2 has-[>svg]:px-3",
         sm: "h-8  gap-1.5 px-3 has-[>svg]:px-2.5 ",
         xs: "h-6  gap-1.5 px-2 has-[>svg]:px-2 text-xs",
         lg: "h-10  px-6 has-[>svg]:px-4",
